69814b91f705d7b7e130652565cb3d58693790e1
yoshero <yosherotagero@gmail.com>
2019-07-01 22:56:34 +0300

    solved 4-3

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,6 +12,24 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if ( pathname.indexOf('/')!== -1 ) {
+        res.statusCode = 400;
+        res.end('Вложенные папки не поддерживаются');
+      } else {
+        try {
+          fs.unlinkSync(filepath);
+          res.statusCode = 200;
+          res.end('Успешно');
+        } catch (e) {
+          if (e.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('Файла не существует');
+          } else {
+            res.statusCode = 500;
+          }
+          res.end('Error');
+        }
+      }
 
       break;
 
