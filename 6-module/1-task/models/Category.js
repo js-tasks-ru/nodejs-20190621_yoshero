d09c0854836e45cbb0c45336761f17de3e9a8b1f
yoshero <yosherotagero@gmail.com>
2019-07-10 23:56:48 +0300

    solved 6-1

--- 6-module/1-task/models/Category.js
+++ 6-module/1-task/models/Category.js
@@ -2,10 +2,32 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
 });
 
 const categorySchema = new mongoose.Schema({
-  subcategories: [subCategorySchema]
+  subcategories: [subCategorySchema],
+  title: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = connection.model('Category', categorySchema);
+
+/*
+Модель категории (`models/Category.js`) должна иметь следующий набор полей:
+
+- `title`, в этом поле будет находиться название категории, например, "Детские товары" или
+"Компьютерная техника".
+    - строковое
+    - обязательное
+- `subcategories`, массив подкатегорий. Каждая подкатегория имеет слеюуще поле:
+    - `title`, в этом поле будет находиться название подкатегории.
+        - строковое
+        - обязательное
+ */
