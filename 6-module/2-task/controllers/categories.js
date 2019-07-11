c24008a0fb29a9d0e7fd654d548d86c60e5de776
yoshero <yosherotagero@gmail.com>
2019-07-12 00:39:57 +0300

    check solved 6-2

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = { categories: [] };
+  const categories = await Category.find();
+  ctx.body = { categories: categories };
 };
--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,38 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+
+
+// продукт по подкатегории
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = { products: [] };
+  const subcat = ctx.query.subcategory;
+  if (!mongoose.Types.ObjectId.isValid(subcat)) {
+    ctx.body = {products: []};
+    ctx.status = 400;
+  } else {
+    const products = await Product.find({subcategory: subcat});
+    ctx.body = {products: products};
+  }
 };
 
+// продукт по
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = { products: [] };
+  const products = await Product.find();
+  ctx.body = {products: products};
 };
 
+// продукт по id
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = { product: {} };
+  if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
+    ctx.body = 'id - not ObjectId';
+    ctx.status = 400;
+  } else {
+    const product = await Product.find( {_id: ctx.params.id});
+    if (product.length === 0) {
+      ctx.body = 'нет товара с таким ID';
+      ctx.status = 404;
+    } else {
+      ctx.body = {product: product[0]};
+    }
+  }
 };
 
