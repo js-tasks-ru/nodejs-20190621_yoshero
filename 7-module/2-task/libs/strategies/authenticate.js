8c70d87a5037acb7b5a5f51a2d4fd298dd567926
yoshero <yosherotagero@gmail.com>
2019-07-13 22:07:22 +0300

    solved 7-2

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,19 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  try {
+    if (!email) return done(null, false, 'Не указан email');
+    const user = await User.findOne({email: email});
+    if (!user) {
+      const newUser = await User.create({
+        email: email,
+        displayName: displayName,
+      });
+
+      return done(null, newUser);
+    }
+    done(null, user);
+  } catch (e) {
+    done(e);
+  }
 };
