53e077b66b5f1b376a1e6a3b6fb367a529b4d921
javascript-ru <47786167+jsru@users.noreply.github.com>
2019-07-18 11:06:34 +0200

    Added task Регистрация с подтверждением email

---
+++ 8-module/1-task/controllers/registration.js
@@ -0,0 +1,11 @@
+const uuid = require('uuid/v4');
+const User = require('../models/User');
+const sendMail = require('../libs/sendMail');
+
+module.exports.register = async (ctx, next) => {
+  /* TODO: */
+};
+
+module.exports.confirm = async (ctx, next) => {
+  /* TODO: */
+};
